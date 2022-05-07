8423b76e91e2c0449542a4a21ddfdb9b071097ba
Fernan R Perez-Galvez <fernan954@gmail.com>
2022-05-07 18:17:02 -0400

    plot size corrected to sample, file managment automatic

--- BATTSI-v2.0.py
+++ BATTSI-v2.0.py
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Apr  2 13:15:30 2022
----
-BATTSI V2.0
----
+
 Biological Activity Transformation Tool
-Version 2.0 - patient pothos
----
-GitHub Repository
-https://github.com/fernan9/BATTSI_V2.0
----
-Update FRPG - Wed Apr 20 2022
--activity trace plots are not displayed correctly
--directory handling not compatible for MacOS (yet)
--missing readme on GitHub
----
+Verison 2.0
+Let's try to run a single script
+
+LOG
+5/7/2022    - plot corrected for a delimited size per sample number
+            - file management automatic with os.path (pathlib not compatible CV2)
+
 @author: Rodrigo Perez
 """
 import cv2
@@ -24,42 +19,45 @@ import numpy as np
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-# Function to extract frames
-def FirstFrame(folder, vidName):
+# Function to extract frames 
+def FirstFrame(folder, vidName): 
     # Path to video file
-    vidObj = cv2.VideoCapture('\\'.join([folder,vidName]))
-    # checks whether frames were extracted
+    vidObj = cv2.VideoCapture(os.path.join(folder, vidName))
+    #vidObj = cv2.VideoCapture('\\'.join([folder,vidName]))
+    # checks whether frames were extracted 
     success = 1
     while success:
-        # vidObj object calls read
-        # function extract frames
+        # vidObj object calls read 
+        # function extract frames 
         success, image = vidObj.read()
         # Saves the frames with frame-count
-        fFrame = "firstFrame-{}.jpg".format(vidName[:-4])
-        imageDir = folder +"\\BATTSI-{}\\{}".format(vidName[:-4],fFrame)
+        fFrameName = "{}-firstFrame.jpg".format(vidName.split('.')[0])
+        midDir = "BATTSI-{}".format(vidName.split('.')[0])
+        imageDir = os.path.join(folder, midDir, fFrameName)
+        #imageDir = folder +"\\BATTSI-{}\\{}".format(vidName.split('.')[0],fFrameName)
         print(imageDir)
         cv2.imwrite(imageDir, image)
         print(" - captured!\n")
         return imageDir
 
 # Function to select a rectangle on image
-def shape_selection(event, x, y, flags, param):
-    # grab references to the global variables
+def shape_selection(event, x, y, flags, param): 
+    # grab references to the global variables 
     global ref_point, crop
-    # if the left mouse button was clicked, record the starting
-    # (x, y) coordinates and indicate that cropping is being performed
-    if event == cv2.EVENT_LBUTTONDOWN:
+    # if the left mouse button was clicked, record the starting 
+    # (x, y) coordinates and indicate that cropping is being performed 
+    if event == cv2.EVENT_LBUTTONDOWN: 
         ref_point = [(x, y)]
-    # check to see if the left mouse button was released
-    elif event == cv2.EVENT_LBUTTONUP:
-        # record the ending (x, y) coordinates and indicate that
-        # the cropping operation is finished
+    # check to see if the left mouse button was released 
+    elif event == cv2.EVENT_LBUTTONUP: 
+        # record the ending (x, y) coordinates and indicate that 
+        # the cropping operation is finished 
         ref_point.append((x, y))
-        # draw a rectangle around the region of interest
-        cv2.rectangle(image, ref_point[0], ref_point[1], (0, 255, 0), 2)
+        # draw a rectangle around the region of interest 
+        cv2.rectangle(image, ref_point[0], ref_point[1], (0, 255, 0), 2) 
         cv2.imshow("BATTSI", image)
 
-# Initiaize some variables
+# Initiaize some variables        
 fFrame=''
 directory = os.getcwd()
 isFile = False
@@ -72,8 +70,9 @@ print("#   SET UP   #")
 videoName = ""
 
 while isFile != True:
-    videoName = str(input('-> Video file (include extension): '))
-    path = '\\'.join([directory,videoName])
+    videoName = str(input('-> Video file (MyVideo.mp4): '))
+    path = os.path.join(directory, videoName)
+    #path = '\\'.join([directory,videoName])
     isFile = os.path.isfile(path)
     if isFile == False:
         print(" - file not found... try again")
@@ -81,12 +80,12 @@ while isFile != True:
         print(" - file found!\n")
 
 print(" - Directory setup...")
-print(" - Initilizing log...")
-
-
+middleDir = "BATTSI-{}".format(videoName.split('.')[0])
 #videoName = "HKDBerea826F4.mp4"
-isExist = os.path.exists(directory  + "\\BATTSI-{}".format(videoName[:-4]) )
-resultsDir = directory + "\\BATTSI-{}".format(videoName[:-4])
+isExist = os.path.exists(os.path.join(directory, middleDir))
+#isExist = os.path.exists(directory  + "\\BATTSI-{}".format(videoName[:-4]) )
+resultsDir = os.path.join(directory,middleDir)
+#resultsDir = directory + "\\BATTSI-{}".format(videoName[:-4])
 if not isExist:
     os.makedirs(resultsDir)
     print(" - Results directory for {} created".format(videoName))
@@ -94,7 +93,7 @@ if not isExist:
 else:
     print(" - Directory for {} results already exists".format(videoName))
     print(" - Results with new parameters will be created...")
-
+    
 
 print("\n - looking for first frame...")
 videoImage = FirstFrame(directory, videoName)
@@ -120,13 +119,13 @@ for i in range(sampleSize):
     a = 'well-%i' %(i+1)
     nombres.append(a)
 #print(nombres)
-# now let's initialize the list of reference point
-ref_point = []
+# now let's initialize the list of reference point 
+ref_point = [] 
 crop = False
 pozos = []
 
 #videoImage = fFrame
-image = cv2.imread(videoImage)
+image = cv2.imread(videoImage) 
 clone = image.copy()
 ventana = ""
 cv2.namedWindow("BATTSI")
@@ -136,33 +135,33 @@ cv2.setMouseCallback("BATTSI", shape_selection)
 for i in nombres:
     #llave = cv2.waitKey(1) & 0xFF
     print("-> Draw ROI for {} <------> Press 'c' to confirm <-".format(i))
-
-    # keep looping until the 'q' key is pressed
-    while True:
-        # display the image and wait for a keypress
-        cv2.imshow("BATTSI", image)
+    
+    # keep looping until the 'q' key is pressed 
+    while True: 
+        # display the image and wait for a keypress 
+        cv2.imshow("BATTSI", image) 
         key = cv2.waitKey(1) & 0xFF
-
-        # press 'r' to reset the window
-        if key == ord("r"):
+      
+        # press 'r' to reset the window 
+        if key == ord("r"): 
             image = clone.copy()
-
-        # if the 'c' key is pressed, break from the loop
-        elif key == ord("c"):
+      
+        # if the 'c' key is pressed, break from the loop 
+        elif key == ord("c"): 
             break
 
-    if len(ref_point) == 2:
+    if len(ref_point) == 2: 
         pozos.append(ref_point)
         print("   - coordinates: {}\n\n     <-> TO continue, press 'c'".format(ref_point))
-
+            
         cv2.waitKey(0)
 
-# close all open windows
+# close all open windows 
 cv2.destroyAllWindows()
 
 print('''
 ------
-
+      
 #  CALIBRATE  #\n
 ------
 ''')
@@ -171,18 +170,18 @@ dilationIter = 1    # dilation iterations
 invalidCalibration = True
 
 while invalidCalibration:
-    calibrate = input('use DEFAULT or MANUAL calibration? (d/m):')
-
+    calibrate = input('use default (d) or manual (m) calibration?:')
+    
     if calibrate == 'm':
         print('''#  MANUAL CALIBRATION  #\n------\nSELECT parameters using toogle bar
         - k - kernel size for Gaussian blurring
         - i - number of dilation iterations\n\nPress C to accept parameters and CONTINUE\n------\n''')
-
+                
         topR = [row[0] for row in pozos]
         botL = [row[1] for row in pozos]
         topR = np.asarray(topR)
         botL = np.asarray(botL)
-
+        
         cap = cv2.VideoCapture(path)
         def trackbar_callback(value, idx):
             global dilationIter
@@ -197,21 +196,21 @@ while invalidCalibration:
         # check for correct file opening
         if(cap.isOpened()==False):
             print ("Error opening video stream or file")
-
+        
         ret, frame1 = cap.read()
         ret, frame2 = cap.read()
-
+        
         # printing loop
         cv2.namedWindow('original', cv2.WINDOW_NORMAL)
         cv2.namedWindow('transformed', cv2.WINDOW_NORMAL)
         cv2.namedWindow('barras', cv2.WINDOW_NORMAL)
         cv2.createTrackbar('i','barras' , 3, 9, lambda v: trackbar_callback(v,0))
         cv2.createTrackbar('k', 'barras', 3, 9, lambda v: trackbar_callback(v,1))
-
-
+        
+        
         while(cap.isOpened()):
             ret,frame = cap.read()
-
+        
             if ret == True:
                 diff = cv2.absdiff(frame1, frame2)
                 gray = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
@@ -234,13 +233,13 @@ while invalidCalibration:
                 #cv2.imshow('todes', todes)
                 cv2.imshow('original', cropped1)
                 cv2.imshow('transformed', cropped3)
-
+                
                 # press C on keyboard to exit
                 if cv2.waitKey(25) & 0xFF == ord('c'):
                     break
             else:
                 break
-
+            
         cap.release()
         cv2.destroyAllWindows()
         print('''------\nIgnore the warnings above!\n------\n - Parameters selected:''')
@@ -250,7 +249,7 @@ while invalidCalibration:
     elif calibrate == 'd' :
         blurKernel = 3      # blur kernel can only be odd
         dilationIter = 3    # dilation iterations
-
+        
         print('''------\nDEFAULT parameters selected:''')
         print("   - Dilation Iteration:{}".format(dilationIter))
         print("   - Blur Kernel:{}".format(blurKernel))
@@ -258,11 +257,11 @@ while invalidCalibration:
     else:
         print(' - Invalid calibration option')
         calibrate = input('use DEFAULT or MANUAL calibration? (d/m):')
-
+        
 
 print('''
 ------
-
+      
 #  TRANSFORMATION  #\n
 ------
 ''')
@@ -293,16 +292,16 @@ while cap.isOpened():
 
         frame1 = frame2
         ret, frame2 = cap.read()
-
+        
         temp2 = []
-
+        
         clone=dilated.copy()
         for j in range(len(pozos)):
             temp = clone[pozos[j][0][1]:pozos[j][1][1],pozos[j][0][0]:pozos[j][1][0]]
             actividad =  np.sum(temp)/(temp.shape[0]*temp.shape[1])
             temp2.append(actividad)
         salida.append(temp2)
-
+        
         i+=1
         if i % 1000 == 0: print(" - {} % done".format(round(i/cuadrosTot*100,2)))
     elif i > cuadrosTot-1 :         # video length should be here
@@ -323,7 +322,7 @@ print("Frame differences analyzed: {}".format(cuadrosTot))
 
 print('''
 ------
-
+      
 #  RESULT SUMMARY  #\n
 ------
 ''')
@@ -333,9 +332,10 @@ print("\n - Plotting figure...")
 df=pd.DataFrame(salida,columns=nombres)
 df['seconds'] = df.index / cuadros
 colnames = list(df.columns)
-barras = df.plot(x='seconds', y=colnames[:-1], subplots = True)
-datasetName = 'BATTSI-{}\\activity-{}-k{}-i{}.pdf'.format(videoName[:-4],videoName[:-4],blurKernel,dilationIter)
-plt.savefig(datasetName)      # k=kernel i=iterations
+barras = df.plot(x='seconds', y=colnames[:-1], figsize=(10,4*sampleSize), subplots = True)
+
+datasetName = '{}-Activity-k{}-i{}.pdf'.format(videoName.split('.')[0],blurKernel,dilationIter)
+plt.savefig(os.path.join(middleDir,datasetName))      # k=kernel i=iterations
 print(" - Figure plotted!\n")
 
 print("------")
@@ -349,9 +349,9 @@ while(knockdownCriteria==False):
     print("     - pixel intensity change (PIC) > 0 %")
     print(" -(m) Set threshold manualy?")
     print("     - Set a threshold PIC percentage (1-100)%")
-
+    
     analysisOption = input(" - Use default (d) or manual (m) threshold?: ")
-
+    
     if analysisOption == 'd':
         criteria = 0
         knockdownCriteria = True
@@ -369,11 +369,11 @@ while(knockdownCriteria==False):
         print(" - Not a valid option, try again!")
 print("\n - Knockdown threshold set to: {}%".format(criteria))
 print(" - Running analysis...")
-# Criteria for determining
+# Criteria for determining 
 knockDown = []
 
 for column in df:
-    s = df[column].tolist()
+    s = df[column].tolist()    
     ls = [k for k, e in enumerate(s) if e > criteria]
     if len(ls) == 0:
         knockDown.append(0)
@@ -383,8 +383,9 @@ for column in df:
 duracion = knockDown.pop()
 
 print(" - Saving dataset...")
-
-df.to_csv('BATTSI-{}\\DataFrame-{}-k{}-i{}.csv'.format(videoName[:-4],videoName[:-4],blurKernel,dilationIter))  # where to save it, usually as a csv
+dataName = '{}-Dataframe-k{}-i{}.csv'.format(videoName.split('.')[0],blurKernel,dilationIter)
+df.to_csv(os.path.join(middleDir, dataName))
+#df.to_csv('BATTSI-{}\\DataFrame-{}-k{}-i{}.csv'.format(videoName[:-4],videoName[:-4],blurKernel,dilationIter))  # where to save it, usually as a csv
 
 print(" -> Saved as CSV file in folder BATTSI-{}\n".format(videoName[:-4]))
 #Then you can load it back using:
@@ -393,16 +394,17 @@ print(" -> Saved as CSV file in folder BATTSI-{}\n".format(videoName[:-4]))
 print(" - Saving results table... (sample, knockdown time (seconds))")
 resultados = {'Well':nombres,'CTmax':knockDown}
 dfctmax = pd.DataFrame(resultados)
-dfctmax.to_csv('BATTSI-{}\\result-{}-k{}-i{}.csv'.format(videoName[:-4],videoName[:-4],blurKernel,dilationIter))
+
+resultName = '{}-Results-k{}-i{}.csv'.format(videoName.split('.')[0],blurKernel,dilationIter)
+dfctmax.to_csv(os.path.join(middleDir,resultName))
 print(" -> Saved as CSV file in folder BATTSI-{}\n".format(videoName[:-4]))
 
-logName = "{}\\BATTSI-{}\\log-{}-i{}-k{}.txt".format(directory,videoName[:-4],videoName[:-4],dilationIter, blurKernel)
+logName = "{}-Log-i{}-k{}.txt".format(videoName.split('.')[0],dilationIter, blurKernel)
 
 # print info to log file
-log = open(logName, 'a')
+log = open(os.path.join(middleDir,logName), 'a')
 
 log.write("--\nBATTSI v2.0 - patient pothos\n--\n - RUN begins\n")
-
 log.write("Video file analyzed: {}\n".format(videoName))
 log.write("Frame rate: {}\n".format(cuadros))
 log.write("Number of frame differences transformed: {}\n--\n".format(cuadrosTot))
